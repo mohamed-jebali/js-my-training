6df68244da6428367a93553c1452b3447ea00701
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-02 21:23:32 +0200

    added clear function

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+let clearAllOnC = document.querySelector("#clearNumber");
+
+function clearNumber (){
+    document.querySelector(".result-container").value = "";
+
+}
+
+clearAllOnC.addEventListener("click",clearNumber);
+
+
+
+
+let clearAllCe = document.querySelector("#clearSingle");
+
+function clearSingleNumber (){
+
+    document.querySelector(".result-container").value
+}
+
+clearAllCe.addEventListener("click",clearSingleNumber);
